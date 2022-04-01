de4b809142ed96d81f7817fd0e794e9930a33c0d
nivedithag24 <102303369+nivedithag24@users.noreply.github.com>
2022-04-01 14:13:42 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import './App.css';
+// import Table from './table.js'
+import Greetings from './greetings'
+
+
+function App() {
+  return (
+    <div className="App">
+     {/* < Table /> */}
+     
+     <Greetings message='hello react developers!' />
+    </div>
+  );
+}
+
+export default App;
---
+++ greetings.js
@@ -0,0 +1,48 @@
+// By using functional component:
+
+// const Greetings = (props) => {
+//     return ( 
+//         <div>
+//             <h1>{props.message}</h1>
+//         </div>
+//      );
+// }
+ 
+// export default Greetings;
+
+
+// By using class component:
+// import React from 'react'
+// class Greetings extends React.Component{
+//     render(){
+//         return(
+//             <div>
+//             <h2>{this.props.message}</h2>
+//         </div>
+//         )        
+//     }
+// }
+// export default Greetings;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
